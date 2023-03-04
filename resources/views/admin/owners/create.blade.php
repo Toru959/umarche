551bb13c6693e5df72d1776b30d76d0b1699c94e
Toru <toru959@gmail.com>
2023-03-04 16:34:24 +0900

    Store

--- resources/views/admin/owners/create.blade.php
+++ resources/views/admin/owners/create.blade.php
@@ -9,6 +9,9 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
+                <x-auth-validation-errors class="mb-4" :errors="$errors" />
+                <form method="post" action="{{route('admin.owners.store')}}">
+                    @csrf
                 <section class="text-gray-600 body-font relative">
                 <div class="container px-5 mx-auto">
                     <div class="flex flex-col text-center w-full mb-12">
@@ -20,13 +23,13 @@
                         <div class="p-2 w-full">
                         <div class="relative">
                             <label for="name" class="leading-7 text-sm text-gray-600">Name</label>
-                            <input type="text" id="name" name="name" required class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                            <input type="text" id="name" name="name" value="{{old('name')}}" required class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
                         </div>
                         </div>
                         <div class="p-2 w-full">
                         <div class="relative">
                             <label for="email" class="leading-7 text-sm text-gray-600">Email</label>
-                            <input type="email" id="email" name="email" required class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                            <input type="email" id="email" name="email" value="{{old('email')}}" required class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
                         </div>
                         </div>
                         <div class="p-2 w-full">
@@ -42,15 +45,14 @@
                         </div>
                         </div>
                         <div class="p-2 w-full flex justify-around mt-10">
-                        <form method="get" action="{{route('admin.owners.index')}}">
-                        <button class="text-white bg-red-500 border-0 py-2 px-8 focus:outline-none hover:bg-red-600 rounded text-lg">戻る</button>
-                        </form>
-                        <button class="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">登録</button>
+                        <button type="button" onclick="location.href='{{route('admin.owners.index')}}'" class="text-white bg-red-500 border-0 py-2 px-8 focus:outline-none hover:bg-red-600 rounded text-lg">戻る</button>
+                        <button type="sbumit" class="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">登録</button>
                         </div>
                     </div>
                     </div>
                 </div>
                 </section>
+                </form>
                 </div>
             </div>
         </div>
--- resources/views/admin/owners/index.blade.php
+++ resources/views/admin/owners/index.blade.php
@@ -11,10 +11,11 @@
                 <div class="p-6 bg-white border-b border-gray-200">
                 <section class="text-gray-600 body-font">
                 <div class="container px-5 mx-auto">
+                <!--<x-flash-message status="info" />-->
                 <div class="flex justify-end mb-4">
-                    <form method="get" action="{{route('admin.owners.create')}}">
-                    <button class="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">新規登録</button>
-                    </form>
+                    
+                    <button onclick="location.href='{{route('admin.owners.create')}}'" class="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">新規登録</button>
+                    
                 </div>
                     <div class="lg:w-2/3 w-full mx-auto overflow-auto">
                     <table class="table-auto w-full text-left whitespace-no-wrap">
