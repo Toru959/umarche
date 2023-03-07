626a1530851b9551ca4c0bc0a0c530eb01fa1f57
Toru <toru959@gmail.com>
2023-03-07 09:36:52 +0900

    shopEditUpdate

--- resources/views/owner/shops/index.blade.php
+++ resources/views/owner/shops/index.blade.php
@@ -21,17 +21,11 @@
                     @endif
                     </div>
                         <div class="text-xl">{{$shop->name}}</div>
-                    <div>
-                        @if(empty($shop->filename))
-                        <img src="{{asset('images/no_image.jpg')}}">
-                        @else
-                        <img src="{{asset('storage/shops/' .$shop->filename)}}">
-                        @endif
+                        <x-shop-thumbnail />
                     </div>
                     </a>
                     </div>
                 @endforeach
-                    </div>
                 </div>
             </div>
         </div>
